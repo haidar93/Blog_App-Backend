4bb97baa5658ed2e6bdd41679d7c7fce1d8c847a
DRAJAT <drajat@DRAJATs-MacBook-Pro.local>
2026-09-09 13:16:59 +0700

    feat: add categories API

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import  express  from "express";
+import express from "express";
 import categoryRoutes from "./routes/categoryRoutes";
 
 const app = express();
 const PORT = 3000;
 
-app.get('/' , (req, res) => {
-    res.send("Hello world")
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello world");
 });
 
 app.use("/categories", categoryRoutes);
 
-app.listen(PORT , () => {
+app.listen(PORT, () => {
     console.log(`server running on http://localhost:${PORT}`);
 });
